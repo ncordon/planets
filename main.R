f47de409ce7a4f42b94639e917b282d912e9fd5f
NCordon <nacho.cordon.castillo@gmail.com>
2016-12-14 17:08:53 +0100

    Separa cálculos del programa principal

--- main.R
+++ main.R
@@ -2,15 +2,22 @@
 
 # Limpieza del entorno
 rm(list = ls())
+# Precisión de 22 dígitos
+options(digits=22)
+# Tolerancia para los algoritmos
+tolerance <- 1e-12
+# Número de puntos a graficar para cada planeta
+n.points <- 100
 
 
 ##########################################################################
 # Lista de paquetes a cargar
 ##########################################################################
 
-pkgs <- c('Rmpfr','Bessel', 'pracma', 'ggplot2')
-
-options(digits=22)
+### Bessel -> funciones de Bessel
+### pracma -> producto vectorial
+### ggplot2 -> paquete para gráficas
+pkgs <- c('Bessel', 'pracma', 'ggplot2')
 
 load.my.packages <- function(){
   to.install <- pkgs[ ! pkgs %in% installed.packages()[,1] ]
@@ -25,82 +32,46 @@ load.my.packages <- function(){
 load.my.packages()
 
 ##########################################################################
-# Carga de algoritmos
+# Carga de archivos auxiliares
 ##########################################################################
+
+# Newton-Raphson y Bessel
 source(file = "algorithm.R")
+# Cálculo de características de planeta(órbita, energía, momento angular,...)
+source(file = "planets.R")
 
 ##########################################################################
 # Lectura de datos
 ##########################################################################
-cat("Introduce el tiempo para el que calcular la posición: ")
-#input<-file('stdin', 'r')
-#t <- as.numeric(readLines(input, n=1))
-
 
 # Leemos los datos de los planetas
 planetas <- read.csv(file='./planetas_data.csv', header= T, stringsAsFactors = F)
+
+# Leemos el tiempo solicitándoselo al usuario
+cat("Introduce el tiempo para el que calcular la posición: ")
+#input<-file('stdin', 'r')
+#t <- as.numeric(readLines(input, n=1))
 t <- 29
 t.ini <- 0
-tolerance <- 1e-12
-n.points <- 100
-
-# Obtenemos la posición para la entrada requerida
-# result.bessel <- bessel.method(a, epsilon, period, t, t.ini, tolerance)
 
 
-# Comprobación de que los resultados con Bessel y Newton Raphson son iguales
-(result.nr - result.bessel) < 1e-10
+##########################################################################
+# Resultados
+##########################################################################
 
+planetas.datos <- lapply(1:nrow(planetas), function(i){ planet.info( planetas[i,] ) })
 
-result <- 
-lapply(1:nrow(planetas), function(i){
-  p <- planetas[i,]
-  name <- p$nombre
-  a <- p$a
-  epsilon <- p$epsilon
-  period <- p$periodo
-  mu <- 4*pi^2/period^2 * a^3
-
-  result.nr <- newton.raphson(a, epsilon, period, t, t.ini, tolerance)
-  posicion.nr <- a*c(cos(result.nr) - epsilon, sqrt(1-epsilon^2)*sin(result.nr))
-  
-  # Extraemos n.points equidistantes en el intervalo para pintar
-  positions <- lapply(0:n.points, function(x){
-    t <- t.ini + x*(period - t.ini)/n.points
-    u <- newton.raphson(a, epsilon, period, t, t.ini, tolerance)
-    c(a*c(cos(u) - epsilon, sqrt(1-epsilon^2)*sin(u)))
-  })
-
-
-  pos.x <- sapply(positions, "[", 1)
-  pos.y <- sapply(positions, "[", 2) 
-
-
-  ##########################################################################
-  # Devolvemos los resultados para la Tierra
-  ##########################################################################
-
-  u.diff <- 2*pi/(period*(1-epsilon * cos(result.nr)))
-  velocidad <- a*c(-sin(result.nr) * u.diff, sqrt(1-epsilon^2) * cos(result.nr) * u.diff)
-  momento.angular = as.vector(cross( c(posicion.nr, 0), c(velocidad, 0) ))
-
-  list(name = name,
-     posicion = posicion.nr,
-     distancia.Sol = sqrt(sum(posicion.nr^2)),
-     velocidad = velocidad,
-     momento.angular = momento.angular,
-     area = 1/2 * sqrt(sum(momento.angular^2)) * (t-t.ini),
-     energia.calculada = sum(velocidad^2)/2 - mu/sqrt( sum(posicion.nr^2) ),
-     energia.teorica = -mu/(2*a),
-     abscisas = pos.x,
-     ordenadas = pos.y)     
-})
-
-positions <- lapply(result, function(p){
+orbitas <- lapply(planetas.datos, function(p){
   data.frame(name = p$name, ordenadas = p$ordenadas, abscisas = p$abscisas)
 })
 
-positions <- do.call(rbind, positions)
+orbitas <- do.call(rbind, orbitas)
 
 
-ggplot(data = positions, aes(x=abscisas, y=ordenadas, col=name)) + geom_point()
+ggplot(data = orbitas, aes(x=abscisas, y=ordenadas, col=name)) +
+  geom_point(size=1) +
+  xlab("x") + ylab("y") +
+  ggtitle("Órbitas de planetas") +
+  scale_color_brewer(palette="Paired") +
+  labs(col = "Planetas")
+ 
---
+++ planets.R
@@ -0,0 +1,54 @@
+##########################################################################
+# Fucnión de cálculo de datos característicos de planeta:
+#     Posición, distancia al sol, momento angular, area de órbita, energía
+##########################################################################
+
+planet.info <- function(planet){
+  name <- planet$nombre
+  a <- planet$a
+  epsilon <- planet$epsilon
+  period <- planet$periodo
+  mu <- 4*pi^2/period^2 * a^3
+
+  # Aproximamos una solución de la ecuación implícita, tanto por Newton-Raphson, como por Bessel
+  posicion <- function(u){
+    a*c(cos(u) - epsilon, sqrt(1-epsilon^2)*sin(u))
+  }
+  result.nr <- newton.raphson(a, epsilon, period, t, t.ini, tolerance)
+  result.bessel <- bessel.method(a, epsilon, period, t, t.ini, tolerance)
+  posicion.nr <- posicion(result.nr)
+  posicion.bessel <- posicion(result.bessel)
+
+  
+  # Extraemos n.points puntos equidistantes en el intervalo para pintar
+  positions <- lapply(0:n.points, function(x){
+    t <- t.ini + x*(period - t.ini)/n.points
+    u <- newton.raphson(a, epsilon, period, t, t.ini, tolerance)
+    c(a*c(cos(u) - epsilon, sqrt(1-epsilon^2)*sin(u)))
+  })
+
+
+  pos.x <- sapply(positions, "[", 1)
+  pos.y <- sapply(positions, "[", 2) 
+
+
+  ##########################################################################
+  # Devolvemos los resultados para la Tierra
+  ##########################################################################
+
+  u.diff <- 2*pi/(period*(1-epsilon * cos(result.nr)))
+  velocidad <- a*c(-sin(result.nr) * u.diff, sqrt(1-epsilon^2) * cos(result.nr) * u.diff)
+  momento.angular = as.vector(cross( c(posicion.nr, 0), c(velocidad, 0) ))
+
+  list(name = name,
+       posicion.nr = posicion.nr,
+       posicion.bessel = posicion.bessel,
+       distancia.sol = sqrt(sum(posicion.nr^2)),
+       velocidad = velocidad,
+       momento.angular = momento.angular,
+       area = 1/2 * sqrt(sum(momento.angular^2)) * (t-t.ini),
+       energia.calculada = sum(velocidad^2)/2 - mu/sqrt( sum(posicion.nr^2) ),
+       energia.teorica = -mu/(2*a),
+       abscisas = pos.x,
+       ordenadas = pos.y)     
+}
